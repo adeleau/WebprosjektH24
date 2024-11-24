0867795ff5fcdbe423ecd2cd2d797cd7203c907a
adeleau <adeleau@stud.ntnu.no>
2024-11-24 16:10:38 +0100

    Fjerna eine register servicen for den var duplisert

--- server/src/services/register-service.ts
+++ server/src/services/register-service.ts
@@ -81,28 +81,6 @@ register(username: string, email: string, password_hash: string): Promise<Users 
     });
 }
 
-    register(username: string, email:string, password_hash:string) {
-        return new Promise<Users | Error> ((resolve, reject) => {
-            pool.query('SELECT * FROM Users WHERE username=? OR email=?', [username, email], (error, results: RowDataPacket[]) => {
-                if (error) return reject(error);
-
-                if (results.length > 0){
-                    return reject(new Error("Username or email already in use"));
-                }
-
-            pool.query('INSERT INTO Users (username, email, password_hash) VALUES (?,?,?)', [username, email, password_hash], (error, results: ResultSetHeader) => {
-                if(error) return reject(error);
-
-                pool.query('SELECT * FROM Users WHERE user_id = ?', [results.insertId], (error, results: RowDataPacket[]) => {
-                    if (error) return reject(error);
-                    resolve(results[0] as Users);
-                })
-            })
-                
-            })
-        })
-    }
-
     checkUserExists(username:string, email:string): Promise<boolean>{
         return new Promise<boolean>((resolve, reject) => {
             pool.query('SELECT COUNT(*) AS count FROM Users WHERE username = ? OR email = ?',
